fcf0a99717cbec631c59661185de9522df79724c
은규 이 <unkyulee@hotmail.com>
2019-05-02 19:01:37 +0200

    push notification setup

--- Build/cordova/build.bat
+++ Build/cordova/build.bat
@@ -1,37 +1,50 @@
+REM Parameter
 SET AngularPath=..\..\Frontend\angular
 
-REM swap index.html with index.html.prod
+REM Swap index.html with index.html.prod
 copy /y %AngularPath%\src\index.mobile.html %AngularPath%\src\index.html
 
-REM build angular app
-pushd %AngularPath%
-call build.bat
-popd
+REM Swap package.json with package.original.json
+del package-lock.json
+copy /y package.original.json package.json
 
-REM remove wwwroot
+REM Remove cordova build folders
 RD /Q /S .\www
 RD /Q /S .\res
 RD /Q /S .\plugins
 RD /Q /S .\platforms
 
-REM create wwwroot
+REM Build Angular
+ECHO build angular app
+pushd %AngularPath%
+REM call build.bat
+popd
 MD .\www
-
-REM copy angular
 xcopy /s /y %AngularPath%\dist .\www
 
+REM Reset angular platform
 call cordova platform remove android
-call cordova platform add android
 
+REM push notification plugin
+call cordova plugin add phonegap-plugin-push --variable SENDER_ID=%1
+
+REM Reset plugin
 call cordova plugin add cordova-plugin-whitelist@latest
 call cordova plugin add cordova-plugin-device@latest
 call cordova plugin add cordova-plugin-camera@latest
 call cordova plugin add phonegap-plugin-barcodescanner@latest
-call cordova plugin remove cordova-plugin-file
+
+REM code push plugin
 call cordova plugin add cordova-plugin-code-push
 call cordova plugin add cordova-plugin-file@latest
 
-REM copy keys
+call cordova platform add android
+
+REM Prepare for the build
 xcopy /s /y .\app\* .\platforms\android
 
+REM Start the build
 call cordova build --release android
+
+REM remove trash files
+del 1
