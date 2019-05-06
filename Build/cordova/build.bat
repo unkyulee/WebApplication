02612c662c924a9d0a529ed3cfe74fe9cad3f573
은규 이 <unkyulee@hotmail.com>
2019-05-06 14:20:55 +0200

    build script for android cordova

--- Build/cordova/build.bat
+++ Build/cordova/build.bat
@@ -6,42 +6,45 @@ copy /y %AngularPath%\src\index.mobile.html %AngularPath%\src\index.html
 
 REM Swap package.json with package.original.json
 del package-lock.json
+del google-services.json
 copy /y package.original.json package.json
 
 REM Remove cordova build folders
 RD /Q /S .\www
-RD /Q /S .\res
 RD /Q /S .\plugins
 RD /Q /S .\platforms
 
 REM Build Angular
 ECHO build angular app
 pushd %AngularPath%
-REM call build.bat
+call build.bat
 popd
 MD .\www
 xcopy /s /y %AngularPath%\dist .\www
 
 REM Reset angular platform
 call cordova platform remove android
+call cordova platform add android
 
-REM push notification plugin
-call cordova plugin add phonegap-plugin-push --variable SENDER_ID=%1
+REM Prepare for the build
+xcopy /s /y .\app\* .\platforms\android
+copy /y .\app\google-services.json google-services.json
 
-REM Reset plugin
+REM common plugin
 call cordova plugin add cordova-plugin-whitelist@latest
 call cordova plugin add cordova-plugin-device@latest
 call cordova plugin add cordova-plugin-camera@latest
 call cordova plugin add phonegap-plugin-barcodescanner@latest
 
+REM push notification plugin
+call cordova plugin add phonegap-plugin-push --variable SENDER_ID=%1
+
 REM code push plugin
 call cordova plugin add cordova-plugin-code-push
 call cordova plugin add cordova-plugin-file@latest
 
-call cordova platform add android
-
-REM Prepare for the build
-xcopy /s /y .\app\* .\platforms\android
+REM apply patch
+xcopy /s /y .\patch\* .\node_modules
 
 REM Start the build
 call cordova build --release android
