4ccb40b9a0ecec22f20460614e16890cd516f21b
은규 이 <unkyulee@hotmail.com>
2019-05-07 18:09:34 +0200

    cordova build script update

--- Build/cordova/build.bat
+++ Build/cordova/build.bat
@@ -21,10 +21,14 @@ call build.bat
 popd
 MD .\www
 xcopy /s /y %AngularPath%\dist .\www
+xcopy /s /y /i .\app\assets\* .\www\assets
 
 REM Reset angular platform
 call cordova platform remove android
 call cordova platform add android
+
+REM Copy config files to the platform
+xcopy /s /y /i .\app\* .\platforms\android\
 
 REM common plugin
 call cordova plugin add cordova-plugin-whitelist@latest
@@ -37,7 +41,6 @@ call cordova plugin add cordova-plugin-code-push
 call cordova plugin add cordova-plugin-file@latest
 
 REM push notification plugin
-xcopy /s /y /i .\app\* .\platforms\android\
 copy /y .\app\google-services.json google-services.json
 call cordova plugin add phonegap-plugin-push --variable SENDER_ID=%1
 
