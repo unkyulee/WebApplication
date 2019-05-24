43da686b00ae68cab0ee3e0774dec501670cc0c4
은규 이 <unkyulee@hotmail.com>
2019-05-24 17:22:15 +0200

    update cordova build on ios

--- Build/cordova/build.sh
+++ Build/cordova/build.sh
@@ -4,33 +4,47 @@ AngularPath=../../Frontend/angular
 # swap index.html with index.html.prod
 cp $AngularPath/src/index.mobile.html $AngularPath/src/index.html
 
+# Swap package.json with package.original.json
+rm package-lock.json
+rm GoogleService-Info.plist
+cp package.original.json package.json
+
+# remove folders
+rm -rf www
+rm -rf plugins
+rm -rf platforms
+
 # build angular app
 pushd $AngularPath
 ng build --prod --aot --build-optimizer
 popd
-
-# remove folders
-rm -R www
-rm -R res
-rm -R plugins
-rm -R platforms
-
-# create wwwroot
 mkdir www
+mkdir www/assets
+cp -R $AngularPath/dist/ www
+cp -R ./app/assets/* www/assets
 
-# copy angular build
-cp -r $AngularPath/dist/ www
+# push notification setup
+cp ./app/GoogleService-Info.plist GoogleService-Info.plist
 
 # add ios platform
 cordova platform remove ios
 cordova platform add ios
 
 # build cordova
-cordova plugin add cordova-plugin-whitelist@latest
-cordova plugin add cordova-plugin-device@latest
-cordova plugin add cordova-plugin-camera@latest
-cordova plugin add phonegap-plugin-barcodescanner@latest
-cordova plugin add cordova-plugin-file@latest
+cordova plugin add cordova-plugin-whitelist
+cordova plugin add cordova-plugin-device
+cordova plugin add cordova-plugin-camera
+cordova plugin add phonegap-plugin-barcodescanner
+
+# code push plugin
+cordova plugin add cordova-plugin-code-push
+cordova plugin add cordova-plugin-file
+cordova plugin add phonegap-plugin-push --variable SENDER_ID=%1
+
+# pod install
+pushd ./platforms/ios
+pod install --verbose
+popd
 
 # build ios app
 cordova run ios
