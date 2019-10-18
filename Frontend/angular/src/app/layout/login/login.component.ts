e1628ab43fe4f27cb4dcc5783243080741278499
unknown <unkyulee@gmail.com>
2019-10-18 21:58:46 +0200

    login screen consistency with basecomponent

--- Frontend/angular/src/app/layout/login/login.component.ts
+++ Frontend/angular/src/app/layout/login/login.component.ts
@@ -1,11 +1,7 @@
-import { Component, OnInit, OnDestroy } from "@angular/core";
-import { Subscription } from "rxjs";
+import { Component } from "@angular/core";
 import * as obj from "object-path";
 
 // user imports
-import { ConfigService } from "../../services/config.service";
-import { AuthService } from "../../services/auth/auth.service";
-import { EventService } from "../../services/event.service";
 import { BaseComponent } from 'src/app/ui/base.component';
 
 @Component({
@@ -13,13 +9,13 @@ import { BaseComponent } from 'src/app/ui/base.component';
   templateUrl: "./login.component.html"
 })
 export class LoginComponent extends BaseComponent {
-  // login screen
-  login: any = {};
-    
+  
   ngOnInit() {
+    obj.ensureExists(this, "data", {})
+    obj.ensureExists(this, "uiElement", {})
+
     // load default login screen
-    if(this.config.get("login"))
-      this.login = this.config.get("login");
+    this.uiElement = this.config.get("login");
 
     // handle events
     this.onEvent = this.event.onEvent.subscribe(async event => {
@@ -36,19 +32,6 @@ export class LoginComponent extends BaseComponent {
 
   // login
   async authenticate() {
-    // validate input
-    this.data.error = "";
-    for (let ui of this.login.screen) {
-      let value = this.data[ui.key]; // used by the evaluation script
-      if(ui.errorCondition) {
-        let error = eval(ui.errorCondition);
-        if (error) this.data.error += `${ui.errorMessage}\n`;
-      }
-    }
-
-    // if there are error don't continue
-    if (this.data.error) return;
-
     // try login
     this.event.send("splash-show"); // show splash
     try {
@@ -60,18 +43,5 @@ export class LoginComponent extends BaseComponent {
       this.data.error = e;
     }
     this.event.send("splash-hide"); // hide splash
-  }
-
-  condition(uiElement) {
-    let result = true;
-    if (uiElement && uiElement.condition) {
-      try {
-        result = eval(uiElement.condition);
-      } catch (e) {
-        console.error(uiElement.condition, e);
-        result = false;
-      }
-    }
-    return result;
   }
 }
