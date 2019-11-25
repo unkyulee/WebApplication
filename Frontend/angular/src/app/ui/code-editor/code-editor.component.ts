de183e08826772be43105374a614b2ed7e2c93cf
unknown <unkyulee@gmail.com>
2019-11-25 13:52:56 +0100

    admin page upgrade

--- Frontend/angular/src/app/ui/code-editor/code-editor.component.ts
+++ Frontend/angular/src/app/ui/code-editor/code-editor.component.ts
@@ -3,6 +3,8 @@ import * as obj from "object-path";
 
 // user Imports
 import { BaseComponent } from "../base.component";
+import { catchError } from "rxjs/operators";
+import { EMPTY } from "rxjs";
 
 @Component({
   selector: "code-editor",
@@ -10,81 +12,150 @@ import { BaseComponent } from "../base.component";
   styleUrls: ["code-editor.component.css"]
 })
 export class CodeEditorComponent extends BaseComponent {
-  path: string;
   error: string;
   compileError: string;
-  type: string;
 
-  _language;
-  get language() {
-    return this._language;
+  ngOnInit() {
+    // subscript to event
+    this.onEvent = this.event.onEvent.subscribe(event =>
+      this.eventHandler(event)
+    );
   }
-  set language(v) {
-    this._language = v;
 
-    // set language option for the editor
-    if (!this.uiElement.editorOptions) this.uiElement.editorOptions = {};
-    this.uiElement.editorOptions = {
-      ...this.uiElement.editorOptions,
-      language: v
-    };
+  ngOnDestroy() {
+    this.onEvent.unsubscribe();
+  }
+
+  eventHandler(event) {
+    if (
+      event &&
+      event.name == "path" &&
+      (!event.key || event.key == this.uiElement.key)
+    ) {
+      // update path
+      this.uiElement.path = event.path;
+    } else if (
+      event &&
+      event.name == "editorOptions" &&
+      (!event.key || event.key == this.uiElement.key)
+    ) {
+      obj.ensureExists(this.uiElement, "editorOptions", {});
+      this.uiElement.editorOptions = {
+        ...this.uiElement.editorOptions,
+        ...event.editorOptions
+      };
+    } else if (
+      event &&
+      event.name == "load-code" &&
+      (!event.key || event.key == this.uiElement.key)
+    ) {
+      this.data = event.data;
+    } else if (
+      event &&
+      event.name == "save" &&
+      (!event.key || event.key == this.uiElement.key)
+    ) {
+      this.save();
+    }
+  }
+
+  save() {
+    // retrieve REST information
+    let src = obj.get(this.uiElement, "save.src");
+    try {
+      src = eval(src);
+    } catch (e) {}
+    let method = obj.get(this.uiElement, "save.method", "post");
+    try {
+      method = eval(method);
+    } catch {}
+
+    // see if any transform to be done
+    let data = { ...this.data };
+    // remove _params_
+    delete data._params_;
+    if (obj.has(this.uiElement, "save.transform")) {
+      try {
+        eval(this.uiElement.save.transform);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+
+    // update data through rest web services
+    if (src) {
+      // hide the splash
+      this.event.send("splash-show");
+
+      this.rest
+        .request(src, data, method)
+        .pipe(
+          catchError(err => {
+            // hide the splash
+            this.event.send("splash-hide");
+
+            // save failed
+            let errorAction = obj.get(this.uiElement, "save.errorAction");
+            if (errorAction) {
+              try {
+                eval(errorAction);
+              } catch (e) {
+                console.error(e);
+              }
+            } else alert(JSON.stringify(err));
+
+            return EMPTY;
+          })
+        )
+        .subscribe(response => this.saveAction(response));
+    } else {
+      // hide splash
+      this.event.send("splash-hide");
+    }
+  }
+
+  saveAction(response) {
+    // hide the splash
+    this.event.send("splash-hide");
+
+    let saveAction = obj.get(this.uiElement, "save.saveAction");
+    if (saveAction)
+      try {
+        eval(saveAction);
+      } catch (e) {
+        console.error(e);
+      }
+    else {
+      // check if the response has error
+      if (response && response.error) {
+        this.snackBar.open(response.error, null, { duration: 2000 });
+      } else {
+        this.snackBar.open("Saved", null, { duration: 2000 });
+      }
+    }
   }
 
   _value;
   get value() {
     // fixed text
-    if (this.uiElement.text) {
-      // set value
-      this._value = this.uiElement.text;
-    }
+    this.error = "";
+    obj.set(this, "uiElement.type", "");
 
-    // key exists
-    else if (this.data && this.uiElement.key) {
-      // set value
-      this._value = this.data[this.uiElement.key];
-    }
+    // remove _params_
+    let data = { ...this.data };
+    delete data._params_;
 
-    // if editor type is json then get value based on the path
-    else if (this.uiElement.editorType == "json") {
-      this.error = "";
-      this.type = "";
-
-      // remove _params_
-      let data = {...this.data};
-      delete data._params_;
-
-      // when path is not specified then display the entire this.data
-      data = obj.get(data, this.path);
-      if (typeof data == "undefined") {
-        // property doesn't exist
-        this.error = "JSON property does not exist.";
-      } else if (data.constructor != "".constructor) {
-        this.type = "object";
-        this._value = JSON.stringify(data, null, 2);
-      } else {
-        this.type = "string";
-        this._value = data;
-      }
-    }
-
-    // if null then assign default
-    if (
-      (typeof this._value == "undefined" || this._value == null) &&
-      this.uiElement.default
-    ) {
-      this._value = this.uiElement.default;
-      try {
-        this._value = eval(this.uiElement.default);
-      } catch (e) {}
-    }
-
-    // if format is specified
-    if (this.uiElement.format) {
-      try {
-        this._value = eval(this.uiElement.format);
-      } catch (e) {
-        console.error(this.uiElement.format, e);
-      }
+    // when path is not specified then display the entire this.data
+    data = obj.get(data, this.uiElement.path);
+    if (typeof data == "undefined") {
+      // property doesn't exist
+      this.error = "JSON property does not exist.";
+    } else if (data.constructor != "".constructor) {
+      this.uiElement.type = "object";
+      this._value = JSON.stringify(data, null, 2);
+    } else {
+      this.uiElement.type = "string";
+      this._value = data;
     }
 
     return this._value;
@@ -93,23 +164,21 @@ export class CodeEditorComponent extends BaseComponent {
   set value(v: any) {
     this.compileError = "";
 
-    if (this._value != v) {
-      this._value = v;
-
-      if (this.data && this.uiElement.key) {
-        this.data[this.uiElement.key] = v;
-      } else if (this.uiElement.editorType == "json") {
-        let data = this._value;
-        if (this.type == "object") {
-          // if the type is object then convert string -> object
-          try {
-            data = JSON.parse(data);
-          } catch(e) {
-            this.compileError = `${e.stack}`;
-            throw e
-          }
+    if(v != this._value) {
+      let data = v;
+      if (this.uiElement.type == "object") {
+        // if the type is object then convert string -> object
+        try {
+          data = JSON.parse(data);
+        } catch (e) {
+          this.compileError = `${e.stack}`;
+          throw e;
         }
-        obj.set(this.data, this.path, data)
+      }
+      if(this.uiElement.path) {
+        obj.set(this.data, this.uiElement.path, data);
+      } else {
+        this.data = data;
       }
     }
   }
