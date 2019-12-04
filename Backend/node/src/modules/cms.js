d432ecb95a5d2a9e26ccf919bf2da6efe8d06177
unknown <unkyulee@gmail.com>
2019-12-04 12:49:41 +0100

    CMS upgrade

--- Backend/node/src/modules/cms.js
+++ Backend/node/src/modules/cms.js
@@ -1,56 +1,64 @@
-const path = require('path')
-const fs = require('fs')
-const ObjectID = require('mongodb').ObjectID
+const path = require("path");
+const fs = require("fs");
+const ObjectID = require("mongodb").ObjectID;
 
-module.exports.requiresAuthentication = async function requiresAuthentication(db, req, res) {
-    return false;
-}
+module.exports.requiresAuthentication = async function requiresAuthentication(
+  db,
+  req,
+  res
+) {
+  return false;
+};
 
 module.exports.process = async function process(db, req, res) {
+  // resolve path
+  let paths = req.path.split("/");
+  if (res.locals.nav.url == "/")
+    // if it is root url then add additional paths
+    paths.unshift("");
 
-    // resolve path
-    let paths = req.path.split('/')
-    if(res.locals.nav.url == '/')
-        // if it is root url then add additional paths
-        paths.unshift('')
+  // get the filename
+  let name = paths[paths.length - 1];
 
-    // get the filename
-    let name = paths[paths.length - 1]
+  // if filename is not given then 'index.html' as default
+  if (!name) {
+    name = "index.html";
+    paths.push("index.html");
+  }
 
-    // if filename is not given then 'index.html' as default
-    if(!name) {
-        name = 'index.html'
-        paths.push('index.html')
-    }
+  // get folder
+  let folder = `/${paths.slice(2, paths.length - 1).join("/")}`;
 
-    // get folder
-    let folder = `/${paths.slice(2, paths.length - 1).join('/')}`
+  // search for contents in cms
+  let results = await db.find("cms", {
+    navigation_id: `${res.locals.nav._id}`,
+    name: name,
+    folder: folder
+  });
+  if (results.length > 0) {
+    return processContent(req, res, results[0]);
+  }
 
-    // search for contents in cms
-    let results = await db.find(
-        'cms'
-        , {
-            navigation_id: `${res.locals.nav._id}`,
-            name: name,
-            folder: folder
-        }
-    )
-    if( results.length > 0 ) return processContent(req, res, results[0])
-
-    // not found
-    res.status(404);
-    res.end();
-    return
-}
+  // not found
+  res.status(404);
+  res.end();
+  return;
+};
 
 async function processContent(req, res, content) {
-    if(content.content_type) {
-        res.header('Content-type', content.content_type)
-    }
+  if(content.headers) {
+   for(let header of content.headers) {
+    res.set(header.key, header.value);
+   }
+  }
 
-    return content.content
+  if (content.type == "binary") {
+    content.content = Buffer.from(content.content, "base64");
+  }
+
+  return content.content;
 }
 
 module.exports.authenticated = async function authenticated(req, res) {
-    return
-}
+  return;
+};
