346a3b1e5d4c3049e9b017b3939538fcab8e0938
Un Kyu Lee <unkyulee@gmail.com>
2020-01-22 10:44:58 +0100

    pdf update

--- Install/mongodb/workflow/PDF.js
+++ Install/mongodb/workflow/PDF.js
@@ -1,42 +1,38 @@
-var Mustache = require("mustache");
-var pdf = require("html-pdf");
-var obj = require("object-path");
+var Mustache = require('mustache');
+var pdf = require('html-pdf');
 
-(async function () {
-    // form values
-    let params = Object.assign({}, req.query, req.body);
+(async function() {
+	// form values
+	let params = Object.assign({}, req.query, req.body);
 
-    // load config
-    await res.locals.ds.connect();
-    let config = await res.locals.ds.find("config", {
-        navigation_id: req.cookies["x-app-key"],
-        pdf: params.pdf
-    });
-    if (config.length == 0) {
-        res.send(`configuration does not exist for ${req.cookies["x-app-key"]} of pdf ${params.pdf}`);
-        res.end();
-        return;
-    }
-    config = config[0];
+	// load config
+	await res.locals.ds.connect();
+	let config = await res.locals.ds.find('config', {
+		query: {
+			company_id: ObjectID(req.cookies['company_id']),
+			type: 'pdf',
+			pdf: params.pdf,
+		},
+	});
+	config = config[0];
+	if (!config) {
+		res.send(`configuration does not exist for ${req.cookies['company_id']} of pdf ${params.pdf}`);
+		res.end();
+		return;
+	}
 
-    // transform
-    let data = await eval(config.data);
+	// transform
+	let data = await eval(config.data);
 
-    // prepare html
-    var html = Mustache.render(config.template, data);
+	// prepare html
+	var html = Mustache.render(config.template, data);
 
-    // convert html to pdf
-    await new Promise(function (resolve, reject) {
-        pdf
-            .create(html, config.options)
-            .toBuffer(function (err, buffer) {
-                res.setHeader(
-                    "Content-disposition",
-                    `inline; filename=${new Date().getTime()}.pdf`
-                );
-                res.end(buffer, "binary");
-                resolve(true);
-            });
-    });
-
+	// convert html to pdf
+	await new Promise(function(resolve, reject) {
+		pdf.create(html, config.options).toBuffer(function(err, buffer) {
+			res.setHeader('Content-disposition', `inline; filename=${new Date().getTime()}.pdf`);
+			res.end(buffer, 'binary');
+			resolve(true);
+		});
+	});
 })();
