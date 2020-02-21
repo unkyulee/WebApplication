e9ada558c3c5c1c475307f0bc0b1a477e1bd90c5
Un Kyu Lee <unkyulee@gmail.com>
2020-02-21 15:43:21 +0100

    window size check is done at every refresh request

--- Frontend/angular/src/app/services/config.service.ts
+++ Frontend/angular/src/app/services/config.service.ts
@@ -1,58 +1,60 @@
-import { Injectable } from "@angular/core";
-import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
-import { Observable } from "rxjs";
-import { map, distinctUntilChanged, debounceTime, first } from "rxjs/operators";
-var obj = require("object-path");
+import { Injectable } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { EventService } from './event.service';
+var obj = require('object-path');
 
 // get config from index.html
 declare var window: any;
 
 @Injectable()
 export class ConfigService {
-  constructor(
-    private breakpointObserver: BreakpointObserver
-  ) {
-    //
-    obj.ensureExists(window, "__CONFIG__", {})
+	constructor(private breakpointObserver: BreakpointObserver, private event: EventService) {
+		//
+		obj.ensureExists(window, '__CONFIG__', {});
 
-    // observe screen size changes
-    this.isHandset$ = this.breakpointObserver
-      .observe([Breakpoints.Handset])
-      .pipe(
-        first(),
-        distinctUntilChanged(),
-        debounceTime(300),
-        map(result => {
-          this.isHandset = result.matches;
-          return result.matches;
-        })
-      );
-    this.isHandset$.subscribe();
-  }
+		// event handler
+		this.event.onEvent.subscribe(e => this.eventHandler(e));
 
-  // detect window size changes
-  public isHandset: boolean;
-  public isHandset$: Observable<boolean>;
+		// observe screen size changes
+		this.checkWindowSize();
+	}
 
-  get(name, def_value?) {
-    let v = null;
-    if (window.__CONFIG__) v = obj.get(window.__CONFIG__, name, def_value);
-    return v;
-  }
+	eventHandler(e) {
+		if (e.name == 'refresh') {
+			this.checkWindowSize();
+		}
+	}
 
-  set(name, value) {
-    // set default if it doesn't exists
-    if (!window.__CONFIG__) window.__CONFIG__ = {};
-    obj.set(window.__CONFIG__, name, value);
-  }
+	// detect window size changes
+	public isHandset: boolean;
+	checkWindowSize() {
+		// observe screen size changes
+		if (this.breakpointObserver.isMatched([Breakpoints.Handset])) {
+			this.isHandset = true;
+		} else {
+			this.isHandset = false;
+		}
+	}
 
-  clear() {
-    // this will reset the ui in the configuration
-    window.__CONFIG__.ui = {}
+	get(name, def_value?) {
+		let v = null;
+		if (window.__CONFIG__) v = obj.get(window.__CONFIG__, name, def_value);
+		return v;
+	}
 
-    // also clear the localStorage
-    let token = localStorage.getItem("token");
-    localStorage.clear();
-    localStorage.setItem("token", token);
-  }
+	set(name, value) {
+		// set default if it doesn't exists
+		if (!window.__CONFIG__) window.__CONFIG__ = {};
+		obj.set(window.__CONFIG__, name, value);
+	}
+
+	clear() {
+		// this will reset the ui in the configuration
+		window.__CONFIG__.ui = {};
+
+		// also clear the localStorage
+		let token = localStorage.getItem('token');
+		localStorage.clear();
+		localStorage.setItem('token', token);
+	}
 }
