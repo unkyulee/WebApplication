6955467a1eb0a4110f8ce4a3ff746d71ce323da4
Un Kyu Lee <unkyulee@gmail.com>
2020-04-08 17:15:42 +0200

    preload when app goes up

--- Frontend/electron/src/window/app/layout/composer.js
+++ Frontend/electron/src/window/app/layout/composer.js
@@ -1,6 +1,6 @@
 const config = require('../../../service/config');
 const event = require('../../../service/event');
-const obj = require("object-path");
+const obj = require('object-path');
 // ui
 require('../ui/ui-element');
 
@@ -16,7 +16,7 @@ Vue.component('Composer', {
 		</div>
 	</div>
 	`,
-	data: function() {
+	data: function () {
 		return {
 			hide: {
 				visibility: 'hidden',
@@ -26,29 +26,69 @@ Vue.component('Composer', {
 			show: {
 				width: '100%',
 				height: '100%',
-				display: "flex",
-				flexFlow: "row wrap"
+				display: 'flex',
+				flexFlow: 'row wrap',
 			},
 			viewports: [],
 			data: {},
 		};
 	},
-	mounted: async function() {
+	mounted: async function () {
+		// listen to login-success and logout event
+		event.subscribe('composer', 'navigation-updated', async () => {
+			// check navs and see if any viewports should be preloaded
+			for (let nav of config.get('nav')) {
+				if (nav.preload) {
+					// create viewport if not exists
+					let viewport = this.viewports.find((x) => x.id == nav.id);
+					if (!viewport) {
+						viewport = {
+							id: nav.id,
+							layoutStyle: {
+								width: '100%',
+								height: '100%',
+							},
+							screens: [
+								{
+									type: 'webview',
+									id: nav.id,
+									src: nav.url,
+									layoutStyle: {
+										width: '100%',
+										height: '100%',
+									},
+								},
+							],
+						};
+						// if the nav has custom screen
+						if (nav.screens) viewport.screens = nav.screens;
+
+						// add viewport
+						viewport.layoutStyle = this.hide;
+						this.viewports.push(viewport);
+					}
+				}
+			}
+
+			// select the first menu
+			this.click(this.navs[0]);
+		});
+
 		// listen to nav-selected
-		event.subscribe('composer', 'nav-selected', nav => {
+		event.subscribe('composer', 'nav-selected', (nav) => {
 			// hide all the screen
 			for (let viewport of this.viewports) {
 				viewport.layoutStyle = this.hide;
 			}
 
 			// create viewport if not exists
-			let viewport = this.viewports.find(x => x.id == nav.id);
-			if(!viewport) {
+			let viewport = this.viewports.find((x) => x.id == nav.id);
+			if (!viewport) {
 				viewport = {
 					id: nav.id,
 					layoutStyle: {
 						width: '100%',
-						height: '100%'
+						height: '100%',
 					},
 					screens: [
 						{
@@ -59,11 +99,11 @@ Vue.component('Composer', {
 								width: '100%',
 								height: '100%',
 							},
-						}
-					]
-				}
+						},
+					],
+				};
 				// if the nav has custom screen
-				if(nav.screens) viewport.screens = nav.screens;
+				if (nav.screens) viewport.screens = nav.screens;
 
 				// add viewport
 				this.viewports.push(viewport);
@@ -71,10 +111,9 @@ Vue.component('Composer', {
 
 			// show the viewport
 			viewport.layoutStyle = this.show;
-
 		});
 	},
-	destroyed: function() {
+	destroyed: function () {
 		// stop listen to drawer-toggle event
 		event.unsubscribe_all('composer');
 	},
