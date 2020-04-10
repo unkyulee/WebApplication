cc772d61881042a770868dd285ec68152f017209
Un Kyu Lee <unkyulee@gmail.com>
2020-04-10 04:15:26 +0200

    google calendar sync

--- Backend/node/src/lib/calendar/provider/google.js
+++ Backend/node/src/lib/calendar/provider/google.js
@@ -1,22 +1,65 @@
 const ObjectID = require('mongodb').ObjectID;
 const obj = require('object-path');
 const axios = require('axios');
+const moment = require('moment');
 
 module.exports = {
 	calendars: async function (db, res, req, params) {
-		// get list of
+		// get access token
 		let token = await this.getToken(db, res, req, params);
 		// get list of calendars
-		let calendars = await axios.get(`https://www.googleapis.com/calendar/v3/users/me/calendarList?minAccessRole=owner`, {
-			headers: { Authorization: `Bearer ${token.access_token}` },
-		});
+		let calendars = await axios.get(
+			`https://www.googleapis.com/calendar/v3/users/me/calendarList?minAccessRole=owner`,
+			{
+				headers: { Authorization: `Bearer ${token.access_token}` },
+			}
+		);
 
 		return obj.get(calendars.data, 'items', []);
 	},
 
-	getEvents: async function (db, res, req, params) {},
+	getEvents: async function (db, res, req, params) {
+		// get access token
+		let token = await this.getToken(db, res, req, params);
+	},
 
-	createEvent: async function (db, res, req, params) {},
+	createEvent: async function (db, res, req, params) {
+		// get access token
+		let token = await this.getToken(db, res, req, params);
+		// see if the calendar is specified
+		if (obj.get(params, 'config.calendar') && obj.get(params, 'event')) {
+			// create an event to the calendar
+			let data = {
+				start: { dateTime: moment(obj.get(params, 'event.appointment_date')).format('YYYY-MM-DDTHH:mm:ssZZ') },
+				end: { dateTime: moment(obj.get(params, 'event.appointment_end_date')).format('YYYY-MM-DDTHH:mm:ssZZ') },
+				summary: obj.get(params, 'event.title'),
+				description: obj.get(params, 'event.note'),
+			};
+
+			if (obj.get(params, 'event.assignees', []).length > 0) {
+				let assignees = obj.get(params, 'event.assignees', []);
+				data.attendees = [];
+				for (let assignee of assignees) {
+					if (assignee.email) {
+						data.attendees.push({
+							displayName: assignee.name,
+							email: assignee.email,
+						});
+					}
+				}
+			}
+
+			let response = await axios.post(
+				`https://www.googleapis.com/calendar/v3/calendars/${obj.get(params, 'config.calendar')}/events`,
+				data,
+				{
+					headers: { Authorization: `Bearer ${token.access_token}` },
+				}
+			);
+
+			return response.data;
+		}
+	},
 
 	updateEvent: async function (db, res, req, params) {},
 
