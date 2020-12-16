757951ad59046bf4106df33c2b72a0d80a23ed64
Un Kyu Lee <unkyulee@gmail.com>
2020-12-16 15:45:35 +0100

    deploy electron version 20

--- Frontend/electron/src/index.js
+++ Frontend/electron/src/index.js
@@ -1,16 +1,4 @@
 if (window.process && window.process.type) {
-  const Store = require("electron-store");
-  window.store = new Store();
-
-  // restore service_url
-  let service_url = window.store.get("service_url");
-  if (!localStorage.getItem("service_url") && service_url)
-    localStorage.setItem("service_url", service_url);
-
-  let token = window.store.get("local.token");
-  if(!localStorage.getItem("token") && token) 
-    localStorage.setItem("token", token);
-
   // handle events
   const { ipcRenderer } = require("electron");
   ipcRenderer.on("channel", (sender, $event) => {
