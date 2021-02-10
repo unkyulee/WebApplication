6a096fb7f0114401017f78ead5b02578c615bce4
Un Kyu Lee <unkyulee@gmail.com>
2021-02-10 10:44:14 +0100

    carousel image layout update

--- Frontend/vue/src/ui/Carousel.vue
+++ Frontend/vue/src/ui/Carousel.vue
@@ -1,15 +1,22 @@
 <template>
-  <v-carousel 
+  <v-carousel
     :style="uiElement.style"
     :class="uiElement.class"
     :height="uiElement.height"
-    :cycle="uiElement.cycle">
-    <v-carousel-item v-for="(slide, i) in uiElement.slides" :key="i">
-      <UiElement          
-          v-bind:key="i"
-          v-bind:uiElement="uiElement.slideTemplate"
-          v-bind:data="slide"
-        />
+    :cycle="uiElement.cycle"
+    :hide-delimiters="uiElement.hideDelimeters"
+  >
+    <v-carousel-item
+      v-for="(slide, i) in uiElement.slides"
+      :key="i"
+      :src="slide.src"
+    >
+      <UiElement
+        v-if="uiElement.slideTemplate"
+        v-bind:key="i"
+        v-bind:uiElement="uiElement.slideTemplate"
+        v-bind:data="slide"
+      />
     </v-carousel-item>
   </v-carousel>
 </template>
