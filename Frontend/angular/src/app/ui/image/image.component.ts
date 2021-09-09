0ef47c31d259995efe21995a5386d669e54d4392
Un Kyu Lee <unkyulee@gmail.com>
2021-09-09 10:00:56 +0200

    iamge error handler

--- Frontend/angular/src/app/ui/image/image.component.ts
+++ Frontend/angular/src/app/ui/image/image.component.ts
@@ -13,8 +13,18 @@ import { BaseComponent } from '../base.component';
 			[ngClass]="uiElement.class"
 			[src]="safeEval(uiElement.src)"
 			(click)="click($event)"
+      (error)="error($event)"
 		/>
 	`,
 })
 export class ImageComponent extends BaseComponent {
+  error(event) {
+    if (this.uiElement && this.uiElement.error) {
+      try {
+        eval(this.uiElement.error);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  }
 }
