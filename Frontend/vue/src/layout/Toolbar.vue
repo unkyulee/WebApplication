86ffec46093199f3cefe2d0bad103bee9dc177b6
Un Kyu Lee <unkyulee@gmail.com>
2022-05-06 10:49:44 +0200

    toolbar configurable

--- Frontend/vue/src/layout/Toolbar.vue
+++ Frontend/vue/src/layout/Toolbar.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app-bar app dense flat :dark="toolbar.dark" :color="toolbar.color">
     <!-- -->
-    <v-app-bar-nav-icon @click.stop="toggleDrawer()"></v-app-bar-nav-icon>
+    <v-app-bar-nav-icon v-if="showMenu" @click.stop="toggleDrawer()"></v-app-bar-nav-icon>
     <v-toolbar-title>{{ title }}</v-toolbar-title>
     <v-spacer></v-spacer>
     <User :data="data" />
@@ -20,20 +20,33 @@ export default {
   components: {
     User,
   },
-  data: function () {
+  data: function() {
     return {
       title: "",
+      showMenu: true,
       toolbar: {
         color: null,
         dark: false,
       },
     };
   },
-  mounted: function () {
+  mounted: function() {
     // load toolbar theme
     this.title = this.config.get("name", "");
     this.toolbar.dark = this.config.get("theme.toolbar.dark", false);
     this.toolbar.color = this.config.get("theme.toolbar.background");
+
+    // subscribe to toolbar-update event
+    this.event.subscribe("Toolbar", "toolbar-update", (event) => {
+      // load toolbar theme
+      this.title = obj.get(event, 'title', this.title);
+      this.toolbar.dark = obj.get(event, 'toolbar.dark', this.toolbar.dark);
+      this.toolbar.color = obj.get(event, 'toolbar.color', this.toolbar.color);
+      if(event.showMenu != 'undefined') this.showMenu = event.showMenu;      
+    });
+  },
+  destroyed: function () {
+    event.unsubscribe_all("Toolbar");
   },
   methods: {
     toggleDrawer() {
