56cfd6d67bb0340d0b8ad408c877e60e40c46f61
Un Kyu Lee <unkyulee@gmail.com>
2022-06-06 16:23:33 +0200

    electron build update

--- Frontend/electron/build.bat
+++ Frontend/electron/build.bat
@@ -9,7 +9,7 @@ REM -------------------------------------------------------------
 REM Build angular app
 REM -------------------------------------------------------------
 pushd ..\angular
-call node --max_old_space_size=8192 ./node_modules/@angular/cli/bin/ng build --prod --aot
+call node --max_old_space_size=8192 ./node_modules/@angular/cli/bin/ng build --aot
 REM call ng build
 popd
 
