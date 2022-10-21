9b1396e93fbb1438a057004ca457a48942af3f68
Un Kyu Lee <unkyulee@gmail.com>
2022-10-21 20:13:11 +0200

    routing base url

--- Frontend/vue3/src/router/index.ts
+++ Frontend/vue3/src/router/index.ts
@@ -1,15 +1,16 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import LayoutEngine from '../layout/LayoutEngine.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import LayoutEngine from "../layout/LayoutEngine.vue";
+import config from "../services/config.service";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(config.get("url")),
   routes: [
     {
-      path: '/:catchAll(.*)',
-      name: 'Layout Engine',
-      component: LayoutEngine
-    }
-  ]
-})
+      path: "/:catchAll(.*)",
+      name: "Layout Engine",
+      component: LayoutEngine,
+    },
+  ],
+});
 
-export default router
+export default router;
