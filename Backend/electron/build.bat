9183f3a88616e66432afc9f961fcb30506f8fbce
Un Kyu Lee <unkyulee@gmail.com>
2022-12-17 02:44:56 +0100

    electron build script

--- Backend/electron/build.bat
+++ Backend/electron/build.bat
@@ -1,6 +1,7 @@
+call npm install --legacy-peer-deps
 REM swap index.html with index.html.prod
-copy /y ..\angular\src\index.electron.html ..\angular\src\index.html
-copy /y .\src\index.js ..\angular\src\index.js
+copy /y ..\..\Frontend\angular\src\index.electron.html ..\..\Frontend\angular\src\index.html
+copy /y .\src\index.js ..\..\Frontend\angular\src\index.js
 
 REM clear wwwroot
 RD /Q /S .\wwwroot
@@ -8,12 +9,12 @@ RD /Q /S .\wwwroot
 REM -------------------------------------------------------------
 REM Build angular app
 REM -------------------------------------------------------------
-pushd ..\angular
-call node --max_old_space_size=8192 ./node_modules/@angular/cli/bin/ng build --aot
-REM call ng build
+pushd ..\..\Frontend\angular
+REM call node --max_old_space_size=8192 ./node_modules/@angular/cli/bin/ng build --aot
+call ng build
 popd
 
 MD .\wwwroot
-xcopy /s /y ..\angular\dist .\wwwroot\
+xcopy /s /y ..\..\Frontend\angular\dist .\wwwroot\
 
 REM call npm run-script build
--- Backend/electron/src/window.js
+++ Backend/electron/src/window.js
@@ -1,106 +1,113 @@
-const { app, BrowserWindow, nativeImage, Menu, dialog, ipcMain } = require('electron');
-const windowStateKeeper = require('electron-window-state');
-const url = require('url');
-const path = require('path');
+const {
+  app,
+  BrowserWindow,
+  nativeImage,
+  Menu,
+  dialog,
+  ipcMain,
+} = require("electron");
+const windowStateKeeper = require("electron-window-state");
+const url = require("url");
+const path = require("path");
 // services
-const config = require('./config');
+const config = require("./config");
 
 module.exports = {
-	window: null,
-	create: async function () {
-		return new Promise((resolve) => {
-			// Load the previous state with fallback to defaults
-			let mainWindowState = windowStateKeeper({
-				defaultWidth: 800,
-				defaultHeight: 600,
-			});
+  window: null,
+  create: async function () {
+    return new Promise((resolve) => {
+      // Load the previous state with fallback to defaults
+      let mainWindowState = windowStateKeeper({
+        defaultWidth: 800,
+        defaultHeight: 600,
+      });
 
-			// create icon from the config
-			let iconDataUri = config.get('module.desktop.icon');
-			let icon = null;
-			if (iconDataUri) icon = nativeImage.createFromDataURL(iconDataUri);
+      // create icon from the config
+      let iconDataUri = config.get("module.desktop.icon");
+      let icon = null;
+      if (iconDataUri) icon = nativeImage.createFromDataURL(iconDataUri);
 
-			// Create the window object
-			this.window = new BrowserWindow({
-				// Set main window title
-				title: config.get('name', 'Loading ...'),
-				// Enable frame if on macOS or if custom titlebar setting is disabled
-				// frame: process.platform !== 'darwin' ? false : true,
-				// Show default title bar on macOS and hide it on others
-				//titleBarStyle: process.platform !== 'darwin' ? 'hidden' : 'default',
-				// Set main window icon
-				icon,
-				webPreferences: {
-					// Enable <webview> tag for embedding
-					webviewTag: true,
-					// Enable nodeIntegration so window can use node functions
-					nodeIntegration: true,
-					webSecurity: false,
-					enableRemoteModule: true,
-				},
-				// Hides main window until it is ready to show
-				show: false,
-				x: mainWindowState.x,
-				y: mainWindowState.y,
-				width: mainWindowState.width,
-				height: mainWindowState.height,
-			});
+      // Create the window object
+      this.window = new BrowserWindow({
+        // Set main window title
+        title: config.get("name", "Loading ..."),
+        // Enable frame if on macOS or if custom titlebar setting is disabled
+        // frame: process.platform !== 'darwin' ? false : true,
+        // Show default title bar on macOS and hide it on others
+        //titleBarStyle: process.platform !== 'darwin' ? 'hidden' : 'default',
+        // Set main window icon
+        icon,
+        webPreferences: {
+          // Enable <webview> tag for embedding
+          webviewTag: true,
+          // Enable nodeIntegration so window can use node functions
+          nodeIntegration: true,
+          webSecurity: false,
+          enableRemoteModule: true,
+        },
+        // Hides main window until it is ready to show
+        show: false,
+        x: mainWindowState.x,
+        y: mainWindowState.y,
+        width: mainWindowState.width,
+        height: mainWindowState.height,
+      });
 
-			// remove menu
-			this.window.removeMenu();
+      // remove menu
+      this.window.removeMenu();
 
-			// Let us register listeners on the window, so we can update the state
-			// automatically (the listeners will be removed when the window is closed)
-			// and restore the maximized or full screen state
-			mainWindowState.manage(this.window);
+      // Let us register listeners on the window, so we can update the state
+      // automatically (the listeners will be removed when the window is closed)
+      // and restore the maximized or full screen state
+      mainWindowState.manage(this.window);
 
-			// redirect message
-			ipcMain.on('channel', (e, msg) => {
-				this.window.webContents.send('channel', msg);
-			});
+      // redirect message
+      ipcMain.on("channel", (e, msg) => {
+        this.window.webContents.send("channel", msg);
+      });
 
-			ipcMain.on('focus', (e, msg) => {
-				this.window.focus();
-			});
+      ipcMain.on("focus", (e, msg) => {
+        this.window.focus();
+      });
 
-			// Main Window Closed Event
-			this.window.on('close', () => {
-				app.quit();
-			});
+      // Main Window Closed Event
+      this.window.on("close", () => {
+        app.quit();
+      });
 
-			// Shows window once ready
-			this.window.once('ready-to-show', async () => {
-				this.window.show();
-				resolve(this.window);
-			});
+      // Shows window once ready
+      this.window.once("ready-to-show", async () => {
+        this.window.show();
+        resolve(this.window);
+      });
 
-			// Load the main window HTML file
-			var args = process.argv.slice(1),
-				serve = args.some(function (val) {
-					return val === '--serve';
-				});
-			if (serve) {
-				this.window.webContents.openDevTools();
-			}
+      // Load the main window HTML file
+      var args = process.argv.slice(1),
+        serve = args.some(function (val) {
+          return val === "--serve";
+        });
+      if (serve) {
+        this.window.webContents.openDevTools();
+      }
 
-			//
-			let debug = false;
-			//this.window.webContents.openDevTools();
-			if(debug) {
-				this.window.loadURL('http://localhost:4200');
-			} else {
-				this.window.loadURL(
-					url.format({
-						pathname: path.join(__dirname, '../wwwroot', 'index.html'),
-						protocol: 'file:',
-						slashes: true,
-					})
-				);
-			}
-		});
-	},
+      //
+      let debug = false;
+      this.window.webContents.openDevTools();
+      if (debug) {
+        this.window.loadURL("http://localhost:4200");
+      } else {
+        this.window.loadURL(
+          url.format({
+            pathname: path.join(__dirname, "../wwwroot", "index.html"),
+            protocol: "file:",
+            slashes: true,
+          })
+        );
+      }
+    });
+  },
 
-	send: function (name, event) {
-		this.window.webContents.send(name, event);
-	},
+  send: function (name, event) {
+    this.window.webContents.send(name, event);
+  },
 };
