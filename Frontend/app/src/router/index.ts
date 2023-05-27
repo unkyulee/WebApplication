6599acf6e37796087ff0d8fb0de097df62175f00
Unkyu Lee <unkyulee@gmail.com>
2023-05-27 16:22:53 +0200

    toast position

--- Frontend/app/src/router/index.ts
+++ Frontend/app/src/router/index.ts
@@ -1,9 +1,9 @@
 // @ts-nocheck
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import Content from "../layout/Content.vue";
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes: [
     {
       path: "/:catchAll(.*)",
