d2c96d36b14acfc5734efbfd74c7eb54d550508e
Un Kyu Lee <unkyulee@gmail.com>
2023-10-12 14:13:42 +0200

    increase the json payload limit

--- Backend/node/index.js
+++ Backend/node/index.js
@@ -29,7 +29,7 @@ app.use(cookieParser());
 
 // process forms
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.json({ limit: 10000000 }));
 
 // process multiparts
 var busboy = require("connect-busboy");
